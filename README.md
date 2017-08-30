c91c61986bafc9aaa42e573c4b2b1ee84e4f873a
Luke Burks <clburks9@gmail.com>
2017-08-29 19:17:20 -0600

    intial commits

---
+++ README.md
@@ -0,0 +1,38 @@
+# Easy POMDP
+<hr>
+Intended to determine how many words in the english language
+a test subject knows.
+
+Words are chosen from the infochimps simple english word list
+on: http://www.infochimps.com/
+
+Words are farther paired down to remove words with numbers,
+resulting in the list used from: https://github.com/dwyl/english-words
+
+## Installation
+
+TODO: Describe the installation process
+
+## Usage
+
+TODO: Write usage instructions
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+TODO: Write history
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+TODO: Write license
---
+++ src/wordCounting.py
@@ -0,0 +1,38 @@
+
+from __future__ import division
+'''
+************************************************************************************************************************************************************
+File: wordCounting.py
+Written By: Luke Burks
+August 2017
+
+Intended to determine how many words in the english language
+a test subject knows.
+
+Words are chosen from the infochimps simple english word list
+on: http://www.infochimps.com/
+
+Words are farther paired down to remove words with numbers,
+resulting in the list used from: https://github.com/dwyl/english-words
+
+
+
+
+
+************************************************************************************************************************************************************
+'''
+
+
+
+__author__ = "Luke Burks"
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Luke Burks"
+__email__ = "clburks9@gmail.com"
+__status__ = "Development"
+
+
+
+
+
+
